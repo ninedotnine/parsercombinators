079bf6dddc18b89f5a92fac5c1cfdc5df8e9bb80
Daniel Soucy <soucydi194@potsdam.edu>
2015-02-22 20:54:34 -0500

    added documentation

--- parsercombinators.py
+++ parsercombinators.py
@@ -4,20 +4,27 @@ position = 0
 input_string = "abcdefghhhhhh[9]"
 
 def getch():
+    """gets the character at the current position"""
     return input_string[position]
 
 def adv():
+    """advances the position in the input by one character"""
     global position
     position += 1
 
 def getpos():
+    """gets the current position in the input stream"""
     return position
 
 def setpos(x):
+    """sets the current position in the input stream"""
     global position
     position = x
 
 def a():
+    """returns 'a' and advances the input stream if the current character
+        matches 'a'
+        otherwise returns failure and does not advance input stream"""
     if getch() == 'a':
         adv()
         return 'a'
@@ -30,8 +37,15 @@ def b():
         return 'b'
     else:
         return failure
+    """returns 'b' and advances the input stream if the current character
+        matches 'b'
+        otherwise returns failure and does not advance input stream"""
 
 def lit(ch): 
+    """returns a function that: 
+        returns ch and advances the input stream if the current character
+        matches ch
+        otherwise returns failure and does not advance input stream"""
     def func():
         ch2 = getch()
         if ch == ch2:
@@ -42,6 +56,10 @@ def lit(ch):
     return func
 
 def pcOr(parser0, parser1):
+    """returns a function that:
+        takes two parsers, runs the first one on the input stream.
+        if that parser returns failure, 
+            we run the second and return the result"""
     def func():
         res0 = parser0()
         if res0 != failure:
@@ -53,6 +71,11 @@ def pcOr(parser0, parser1):
     return func
 
 def pcAnd(parser0, parser1):
+    """returns a function that:
+        takes two parsers, runs both on the input stream.
+        if either parser returns failure, 
+            return failure without advancing the input stream.
+        otherwise return a list containing both results"""
     def func():
         pos = getpos()
         res0 = parser0()
@@ -67,6 +90,12 @@ def pcAnd(parser0, parser1):
     return func
 
 def pcMany(parser):
+    """returns a function that:
+        takes one parser and runs it on the input stream.
+        if the parser returns failure, 
+            return failure without advancing the input stream.
+        otherwise run the parser again until it returns failure, 
+        then return a list containing all results"""
     def func():
         res = parser()
         if res == failure:
@@ -80,6 +109,11 @@ def pcMany(parser):
     return func
 
 def pcBetween(parser1, parser2, parser3):
+    """returns a function that:
+        takes three parsers and runs all of them on the input stream.
+        if any parser returns failure, 
+            return failure without advancing the input stream.
+        otherwise return the return value of the middle parser"""
     def func():
         start = parser1()
         if start == failure:
@@ -94,6 +128,13 @@ def pcBetween(parser1, parser2, parser3):
     return func
 
 def pcApply(f, parser):
+    """returns a function that:
+        takes one function and one parser 
+        and runs the parser on the input stream.
+        if the parser returns failure, 
+            return failure without advancing the input stream.
+        otherwise return the result of applying the function to
+            the return value of the middle parser"""
     def func():
         res = parser()
         if res == failure:
@@ -103,6 +144,7 @@ def pcApply(f, parser):
     return func
 
 def main():
+    # for testing and demo purposes
     parser = a
     print(parser())
     print(b())
